8d7261ea20e7419fd556a84ab715dfb165a969ac
GustavoHiguera <ghiguera@ucol.mx>
2022-08-17 18:45:29 -0500

    Add last frame exceptions
    -Add third_try variable
    -Add if's to random_try and calculate_totals
    
    Co-authored-by: dgentner <dgentner@ucol.mx>
    Co-authored-by: elioflo <eliofloreschoque2357@gmail.com>

--- app/frame.rb
+++ app/frame.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class Frame
-  attr_accessor :next_frame, :previous_frame, :first_try, :second_try, :total
+  attr_accessor :next_frame, :previous_frame, :first_try, :second_try, :total, :third_try
 
   def initialize
     @total = 0
     @first_try = 0
     @second_try = 0
+    @third_try = nil
     @next_frame = nil
     @previous_frame = nil
   end
@@ -21,13 +22,17 @@ class Frame
 
   def calculate_total
     subtotal = @previous_frame ? @previous_frame.total : 0
-    if is_strike?
+    if is_strike? && @next_frame.nil?
+      subtotal += @first_try + @second_try + @third_try
+    elsif is_strike?
       subtotal += 10
       subtotal += if @next_frame.is_strike?
                     @next_frame.first_try + @next_frame.next_frame.first_try
                   else
                     @next_frame.first_try + @next_frame.second_try
                   end
+    elsif is_spare? && @next_frame.nil?
+      subtotal += 10 + @third_try
     elsif is_spare?
       subtotal += 10 + @next_frame.first_try
     else
--- app/game.rb
+++ app/game.rb
@@ -1,46 +1,60 @@
 # frozen_string_literal: true
-require "./frame.rb"
+
+require './frame'
 
 class Game
-
   def initialize
-    @game = Array.new(10) {Frame.new}
+    @game = Array.new(10) { Frame.new }
   end
 
   def start
     @game.each_with_index do |frame, i|
-      if(i <= 0)
+      if i <= 0
         frame.next_frame = @game[i + 1]
-      elsif(i >= 9)
+      elsif i >= 9
         frame.previous_frame = @game[i - 1]
       else
         frame.next_frame = @game[i + 1]
         frame.previous_frame = @game[i - 1]
       end
-      random_try(frame)
+      random_try(frame, i)
     end
-    calculate_totals()
+
+    calculate_totals
   end
 
-  def random_try(frame)
+  def random_try(frame, i)
     r1 = rand(11)
     frame.first_try = r1
 
-    if(r1 != 10)
-      r2 = rand(11-r1)
+    if r1 != 10
+      r2 = rand(11 - r1)
       frame.second_try = r2
+    end
+
+    if i == 9 && r1 == 10
+      r2 = rand(11)
+      frame.second_try = r2
+
+      r3 = rand(11)
+      frame.third_try = r3
+    elsif i == 9 && frame.is_spare?
+      r3 = rand(11)
+      frame.third_try = r3
     end
   end
 
   def calculate_totals
     @game.each do |frame|
-      puts "- - - - - - - - -"
-      puts "#{frame.first_try} #{frame.second_try}"
+      puts '- - - - - - - - -'
+      if !@game[9]
+        puts "#{frame.first_try} #{frame.second_try}"
+      else
+        puts "#{frame.first_try} #{frame.second_try} #{frame.third_try}"
+      end
       puts frame.calculate_total
     end
   end
-
-
 end
 
 ngame = Game.new
