016028434e1afd84ec8c94f1a1d2fde72989f6c6
elioflo <eliofloreschoque2357@gmail.com>
2022-08-17 18:56:45 -0300

    Add class Frame.
    co-authored-by: GustavoHiguera <ghiguera@ucol.mx>
    co-authored-by: dgentner <dgentner@ucol.mx>

---
+++ app/frame.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Frame
+  attr_accessor :next_frame, :previous_frame, :first_try, :second_try, :total
+
+  def initialize
+    @total = 0
+    @first_try = 0
+    @second_try = 0
+    @next_frame = nil
+    @previous_frame = nil
+  end
+
+  def is_strike?
+    @first_try == 10
+  end
+
+  def is_spare?
+    (@first_try + @second_try) == 10
+  end
+
+  def calculate_total
+    subtotal = @previous_frame ? @previous_frame.total : 0
+    if is_strike?
+      subtotal += 10
+      subtotal += if @next_frame.is_strike?
+                    @next_frame.first_try + @next_frame.next_frame.first_try
+                  else
+                    @next_frame.first_try + @next_frame.second_try
+                  end
+    elsif is_spare?
+      subtotal += 10 + @next_frame.first_try
+    else
+      subtotal += @first_try + @second_try
+    end
+    @total = subtotal
+  end
+end
