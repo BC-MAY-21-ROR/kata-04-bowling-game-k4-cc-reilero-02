8f339137352ba473136cdbb3aae67dfae67f0e99
dagp-ucol <dgentner@ucol.mx>
2022-08-17 17:58:26 -0500

    Add class Game
    - Initialize double linked list
    - Add randomizer to Frame tries
    - Add calculate_totals function to print game
    
    Co-authored-by: GustavoHiguera <ghiguera@ucol.mx>
    Co-authored-by: elioflo <eliofloreschoque2357@gmail.com>

--- app/game.rb
+++ app/game.rb
@@ -1,4 +1,47 @@
 # frozen_string_literal: true
+require "./frame.rb"
 
 class Game
+
+  def initialize
+    @game = Array.new(10) {Frame.new}
+  end
+
+  def start
+    @game.each_with_index do |frame, i|
+      if(i <= 0)
+        frame.next_frame = @game[i + 1]
+      elsif(i >= 9)
+        frame.previous_frame = @game[i - 1]
+      else
+        frame.next_frame = @game[i + 1]
+        frame.previous_frame = @game[i - 1]
+      end
+      random_try(frame)
+    end
+    calculate_totals()
+  end
+
+  def random_try(frame)
+    r1 = rand(11)
+    frame.first_try = r1
+
+    if(r1 != 10)
+      r2 = rand(11-r1)
+      frame.second_try = r2
+    end
+  end
+
+  def calculate_totals
+    @game.each do |frame|
+      puts "- - - - - - - - -"
+      puts "#{frame.first_try} #{frame.second_try}"
+      puts frame.calculate_total
+    end
+  end
+
+
 end
+
+ngame = Game.new
+ngame.start
